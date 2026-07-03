a11c54b92497b45d1ec7b93f5a2fcdfa3c7e8c4a
Mohit Choudhari <mohitchoudhari162@gmail.com>
2026-07-03 08:41:31 +0530

    Redis connection

---
+++ config/redis.js
@@ -0,0 +1,13 @@
+import { createClient } from 'redis';
+
+const client = createClient({
+    url: 'redis://localhost:6379'
+});
+
+client.on('error', (err) => {
+    console.log(err);
+});
+
+await client.connect();
+
+export default client;
